379f6e9effb84e035a8f1f1dd0556828f2df6ffd
Youssef  Khalil <ykhalil-@c2r6p7.1337.ma>
2026-03-26 12:02:23 +0100

    second

---
+++ ex01/Program.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = (int)scanner;
+
+        if (number <= 1) {
+            System.err.println("IllegalArgument");
+            System.exit(-1);
+        }
+
+        boolean isPrime = true;
+        int iterations = 0;
+
+        for (int i = 2; i <= number / 2; i++) {
+            iterations++; // count comparison
+
+            if (number % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        System.out.println(isPrime + " " + iterations);
+    }
+}
